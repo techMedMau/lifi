3eb57d24b1ac065669964b0c5b5065ac3222e311
Maureen Chang <tz14pon@gmail.com>
2022-01-09 01:23:17 +0800

    invoice list

--- src/components/invoice.vue
+++ src/components/invoice.vue
@@ -1,10 +1,18 @@
 <template>
   <div class="invoice">
-    <div>
-      <p>{{date}}</p>
-      <div></div>
+    <div class="invoice_right">
+      <div>
+        <p class="invoice_date">{{date}}</p>
+        <div class="invoice_tag" :style="{'background-color': invoice.status==='驗證中'?'#FF5C5C':'#FFCE33', 'color': invoice.status==='驗證中'?'white':'#37375A'}">{{invoice.status==='驗證中'? '驗證中':invoice.type === 1? '載具':'電子'}}</div>
+      </div>
+      <div class="invoice_product">
+        <p class="invoice_product_display">{{displayInfo}}</p>
+        <p class="invoice_product_seller">{{invoice.sellerName? invoice.sellerName:'無店家資料'}}</p>
+      </div>
     </div>
-    <div></div>
+    <div class="invoice_left">
+      {{invoice.amount? invoice.amount:'--' }}元
+    </div>
   </div>
 </template>
 
@@ -28,15 +36,67 @@ export default {
   computed:{
     date(){
       return this.invoice.time.split('-')[1]+'/'+this.invoice.time.substring(8,10)
+    },
+    displayInfo(){
+      if(this.invoice.status === '驗證中'){
+        return this.invoice.invNum
+      }else{
+        return this.invoice.details[0].description
+      }
     }
   }
 }
 </script>
 
-<style>
+<style lang="scss">
 .invoice{
+  height: max-content;
   width: 100%;
   background-color: white;
   padding: 12px 10px 13px 8px;
+  display: flex;
+  justify-content: space-between;
+  & p {
+    margin: 0;
+  }
+  & div+div{
+    margin-left: 9px;
+  }
+  &_right{
+    display: flex;
+  }
+  &_left{
+    font-size: 14px;
+    line-height: 21px;
+    display: flex;
+    align-items: center;
+  }
+  &_date{
+    font-size: 12px;
+    line-height: 18px;
+    color: #5A5A78;
+    text-align: center;
+  }
+  &_tag{
+    font-size: 10px;
+    line-height: 15px;
+    text-align: center;
+    border-radius: 8px;
+    // align-items: center;
+    width: 36px;
+    height: 16px;
+  }
+  &_product{
+    &_display{
+      color: #37375A;
+      font-size: 14px;
+      line-height: 21px;
+    }
+    &_seller{
+      color: #999999;
+      font-size: 10px;
+      line-height: 15px;
+    }
+  }
 }
 </style>
